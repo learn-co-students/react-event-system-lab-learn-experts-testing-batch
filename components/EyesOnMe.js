9fc5c7017dc9af3ac0f47fddd95d44945343f8f4
pajamaw <pjpeterww@gmail.com>
2016-12-14 09:31:48 -0500

    Done.

---
+++ components/EyesOnMe.js
@@ -0,0 +1,28 @@
+import React from 'react';
+
+class EyesOnMe extends React.Component {
+  constructor(){
+    super();
+
+    this.onFocus = this.onFocus.bind(this);
+    this.onBlur = this.onBlur.bind(this);
+  }
+
+  onFocus(){
+    console.log('Good!')
+  }
+  onBlur(){
+    console.log('Hey! Eyes on me!')
+  }
+  render(){
+    return (
+      <div>
+      <button onFocus={this.onFocus} onBlur={this.onBlur}>
+      oi
+      </button>
+      </div>
+    );
+  }
+}
+
+module.exports = EyesOnMe;
